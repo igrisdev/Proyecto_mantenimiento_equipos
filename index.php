7ba5ed83b5313aba4e9b08d27c5f53e14f3f5f38
igrisdev <johanalvarezz237@gmail.com>
2024-04-21 15:42:07 -0500

    desacoplando el index, para que se pueda reutilizar el head y foot

--- index.php
+++ index.php
@@ -1,30 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include("./components/head.php") ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Proyecto De Mantenimiento De Equipos</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Fondo -->
+<div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+  <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
+</div>
 
-  <script src="https://cdn.tailwindcss.com"></script>
-  <link rel="stylesheet" href="styles/style.css">
-</head>
+<!-- header -->
+<?php include("components/ui/cruds.php") ?>
 
-<body class="relative min-h-screen">
+<!-- main -->
+<?php include("components/main.php") ?>
 
-  <!-- Fondo -->
-  <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-    <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
-  </div>
-
-  <!-- header -->
-  <?php include("components/navbar.php") ?>
-
-  <!-- main -->
-  <?php include("components/main.php") ?>
-
-  <script src="js/script.js"></script>
-</body>
-
-</html>
+<?php include("./components/foot.php") ?>
