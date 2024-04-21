0d2c88e20ae6fbb38645b1749a53fb6006bb6118
igrisdev <johanalvarezz237@gmail.com>
2024-04-21 15:47:41 -0500

    add campos de tabla nuevos

--- lib/objetoInfoTablas.php
+++ lib/objetoInfoTablas.php
@@ -4,7 +4,7 @@ $tablas = [
   ['nombre' => 'Sedes', 'columnas' => ['Id', 'Nombre'], 'id' => 'sedes'],
   ['nombre' => 'Salas', 'columnas' => ['Id', 'Nombre', 'Sede'], 'id' => 'salas'],
   ['nombre' => 'Marcas', 'columnas' => ['Id', 'Nombre'], 'id' => 'marcas'],
-  ['nombre' => 'Equipos', 'columnas' => ['Id', 'Tipo', 'Marca', 'Sala'], 'id' => 'equipos'],
+  ['nombre' => 'Equipos', 'columnas' => ['Id', 'Tipo', 'Marca', 'Sala', 'Código', 'Fecha Ingreso', 'Estado'], 'id' => 'equipos'],
   ['nombre' => 'Monitores', 'columnas' => ['Id' => 'Cédula', 'Nombre'], 'id' => 'monitores'],
-  ['nombre' => 'Mantenimiento', 'columnas' => ['Id', 'Tipo Mantenimiento', 'Problema', 'Descripción', 'Fecha Creación', 'Equipo', 'Monitor'], 'id' => 'mantenimientos'],
+  ['nombre' => 'Mantenimiento', 'columnas' => ['Id', 'Tipo Mantenimiento', 'Problema', 'Descripción', 'Fecha Creación', 'Equipo', 'Monitor', 'Fecha Fin'], 'id' => 'mantenimientos'],
 ];
