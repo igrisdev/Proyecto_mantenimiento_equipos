806b52ae8055f74e96ee2176ff79063c9d789ce0
igrisdev <johanalvarezz237@gmail.com>
2024-04-22 01:36:11 -0500

    update create mantenimiento

--- api/crear/crear__mantenimientos.php
+++ api/crear/crear__mantenimientos.php
@@ -4,8 +4,19 @@ include("../../db/database.php");
 
 $conn = connection();
 
+if (!isset($_POST['idEquipo'])) {
+  echo "Falta el id ";
+  $conn->close();
+  return;
+}
+
 if (!isset($_POST['tipo_mantenimiento'])) {
   echo "Falta el Tipo De Mantenimiento";
+  $conn->close();
+  return;
+}
+if (!isset($_POST['fecha_inicio'])) {
+  echo "Falta la Fecha de Inicio";
   $conn->close();
   return;
 }
@@ -16,35 +27,26 @@ if (!isset($_POST['problema'])) {
   return;
 }
 
-if (!isset($_POST['descripcion'])) {
-  echo "Falta la Descripción";
-  $conn->close();
-  return;
-}
-
-if (!isset($_POST['idEquipo'])) {
-  echo "Falta el id ";
-  $conn->close();
-  return;
-}
-
 if (!isset($_POST['quienCC'])) {
   echo "Falta la Sede";
   $conn->close();
   return;
 }
 
-$tipo_mantenimiento = $_POST['tipo_mantenimiento'];
-$problema = $_POST['problema'];
-$descripcion = $_POST['descripcion'];
 $idEquipo = $_POST['idEquipo'];
+$tipo_mantenimiento = $_POST['tipo_mantenimiento'];
+$fecha_inicio = $_POST['fecha_inicio'];
+$problema = $_POST['problema'];
 $quienCC = $_POST['quienCC'];
 
-$query = "INSERT INTO `mantenimientos` (`id`, `tipo_mantenimiento`, `problema`, `descripcion`, `idEquipo`, `quienCC`) VALUES (NULL, '$tipo_mantenimiento', '$problema', '$descripcion', '$idEquipo', '$quienCC');";
+$query = "INSERT INTO `mantenimientos` (`id`, `tipo_mantenimiento`, `fecha_inicio`, `problema`, `idEquipo`, `quienCC`) VALUES (NULL, '$tipo_mantenimiento', '$fecha_inicio', '$problema', '$idEquipo', '$quienCC');";
+
+$query2 = "UPDATE `equipos` SET  `estado` = '1' WHERE `equipos`.`id` = $idEquipo;";
 
 try {
   $conn->query($query);
-  echo "Mantenimiento Creado";
+  $conn->query($query2);
+  echo "Mantenimiento Creado para el equipo $idEquipo";
   // } catch (\Throwable $th) {
 } catch (mysqli_sql_exception $exception) {
   echo "Error al crear el Mantenimiento" . $exception;
