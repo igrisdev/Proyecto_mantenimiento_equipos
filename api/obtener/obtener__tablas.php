f59cccd43f9b4f35e1de046b87885ff3e91767c4
igrisdev <johanalvarezz237@gmail.com>
2024-04-23 15:41:22 -0500

    mejorando el sql de equipos

--- api/obtener/obtener__tablas.php
+++ api/obtener/obtener__tablas.php
@@ -12,7 +12,13 @@ if (!isset($_POST['tabla'])) {
 
 $tabla = $_POST['tabla'];
 
-$query = "SELECT * FROM `$tabla`";
+if ($tabla === "equipos") {
+  $query = "SELECT equipos.id, equipos.codigo, equipos.tipo, equipos.idMarca, salas.nombre, salas.idSede, equipos.fecha_ingreso as 'fecha ingreso', IF(mantenimientos.fecha_inicio IS NOT NULL, mantenimientos.fecha_inicio, '') as 'ultimo mantenimiento', if(mantenimientos.fecha_inicio IS NOT NULL, DATE_ADD(mantenimientos.fecha_inicio, INTERVAL 6 MONTH), DATE_ADD(equipos.fecha_ingreso, INTERVAL 6 MONTH)) as 'siguiente mantenimiento', equipos.estado FROM equipos LEFT JOIN salas ON equipos.idSala = salas.id LEFT JOIN mantenimientos ON equipos.id = mantenimientos.idEquipo";
+}
+
+if ($tabla !== "equipos") {
+  $query = "SELECT * FROM `$tabla`";
+}
 
 try {
   $result = $conn->query($query);
